a09a9534c22d33ec70f82bcdf578c1d7223c702d
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-05-13 00:46:17 +0300

    Стилизованная компонента Logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+        <a href="">
+            <Icon
+                iconId={'codeReact'}
+                codeTelegram={'codeTelegram'}
+                codeVK={'codeVK'}
+            />
+        </a>
+    );
+};
+
